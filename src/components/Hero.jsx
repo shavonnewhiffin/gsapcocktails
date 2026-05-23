869cf9f0e81d0fcfe99f8026c154e6802945955a
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-05-22 18:26:38 -0700

    implemented cocktails section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,8 +46,8 @@ const Hero = () => {
         .to('.right-leaf', { y: 200 }, 0)
         .to('.left-leaf', { y: -200 }, 0)
 
-        const startValue = 'top top';
-        const endValue = 'bottom top';
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
 
         let tl = gsap.timeline({
             scrollTrigger: {
