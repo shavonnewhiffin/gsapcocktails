5afc0873ee8c48954e2174a5e39db7df041a4fe3
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-05-23 17:29:41 -0700

    small changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ useGSAP(() => {
       <div>
         <a href="#home" className="flex items-center gap-2">
             <img src="/images/logo.png" alt="logo" />
-          <p>Velvet Pour</p>
+          <p>Mojito</p>
         </a>
 
         <ul>
